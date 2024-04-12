5738a2f9cf8d9bf234398b51b9306df0e1697016
Vita.19333 <Vita.19333@LABI1-6PC21>
2024-04-12 10:12:33 +0200

    Aggiungere i file di progetto.

---
+++ ClientRESTAPI/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace F1CircuitClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(617, 199);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(403, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(530, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(956, 527);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewCircuits;
+        private System.Windows.Forms.TextBox txtCircuitName;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ClientRESTAPI/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F1CircuitClient
+{
+    public partial class Form1 : Form
+    {
+        private const string BaseUrl = "http://localhost/path/to/php_script.php/";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            await RefreshCircuits();
+        }
+
+        private async Task RefreshCircuits()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(BaseUrl + "circuits");
+                    response.EnsureSuccessStatusCode(); 
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    List<Circuit> circuits = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Circuit>>(responseBody);
+
+
+                    listViewCircuits.Items.Clear();
+                    foreach (Circuit circuit in circuits)
+                    {
+                        ListViewItem item = new ListViewItem(circuit.CircuitId.ToString());
+                        item.SubItems.Add(circuit.Name);
+                        listViewCircuits.Items.Add(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtCircuitName.Text;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    var data = new Dictionary<string, string>
+                    {
+                        { "name", name }
+                    };
+                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(data);
+                    HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync(BaseUrl + "circuits/add", content);
+                    response.EnsureSuccessStatusCode();
+
+                    MessageBox.Show("Circuit added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    await RefreshCircuits();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listViewCircuits.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Please select a circuit to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                int circuitId = int.Parse(listViewCircuits.SelectedItems[0].Text);
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.DeleteAsync(BaseUrl + $"circuits/delete/{circuitId}");
+                    response.EnsureSuccessStatusCode();
+
+                    MessageBox.Show("Circuit deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    await RefreshCircuits();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Circuit
+    {
+        public int CircuitId { get; set; }
+        public string Name { get; set; }
+    }
+}
